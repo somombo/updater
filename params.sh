78c9d78e61bea9785731042cb3bc61f471748681
Somo S <somo@mombo.solutions>
2020-04-15 15:23:22 -0400

    add error code help

--- params.sh
+++ params.sh
@@ -16,13 +16,28 @@ if [[ $params ]]; then
 	fi
 
 	if [[ " ${params[@]} " =~ " -h " ]] || [[ " ${params[@]} " =~ " --help " ]]; then
-    echo
-    echo "  Example usage: "
-		echo "    upd      # To run update"
-		echo "    upd -v   # For current version. Or you can use \`upd --version\`"
-		echo "    upd -h   # For help. Or you can use \`upd --help\`"
-		echo
-		echo
+cat << 'EOF'
+
+  Example usage: 
+    upd      # To run update
+    upd -v   # For current version. Or you can use \`upd --version\`
+    upd -h   # For help. Or you can use \`upd --help\`
+
+  Error codes:
+    02  # Could not chocolatey upgrade nodejs 
+    03  # Could not upgrade npm 
+    05  # Could not update global npm packages
+    07  # Could not chocolatey upgrade miniconda3
+    11  # Could not activate conda
+    13  # Could not conda update conda package
+    17  # Could not conda update all packages
+    19  # Could not conda install console and powershell shortcuts
+    23  # Could not deactivate conda
+    29  # Could not chocolatey upgrade all packages
+    31  # Could not wsl apt-get update and upgrade
+
+
+EOF
 	else
 		err_code=$(($err_code + 1))
 	fi
