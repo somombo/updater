b7ec023a363993c1a44d9ec635cb9af68b3aaa2a
Somo S <somo@balena.io>
2022-05-23 14:44:26 +0000

    upgrade git for windows seperately earlier
    Signed-off-by: Somo S <somo@balena.io>

--- params.sh
+++ params.sh
@@ -27,17 +27,18 @@ cat << 'EOF'
     02  # Could not chocolatey upgrade nodejs 
     03  # Could not upgrade npm 
     05  # Could not update global npm packages
-    07  # Could not chocolatey upgrade miniconda3
-    11  # Could not activate conda
-    13  # Could not conda update conda package
-    17  # Could not conda update all packages
-    19  # Could not conda install console and powershell shortcuts
-    23  # Could not deactivate conda
-    29  # Could not chocolatey upgrade all packages
-    31  # Could not update wsl kernel
-    37  # Could not apt update and apt upgrade in wsl
-    41  # Could not update Rust toolchain
-    43  # Could not upgrade Deno
+    07  # Could not upgrade git
+    11  # Could not chocolatey upgrade miniconda3
+    13  # Could not activate conda
+    17  # Could not conda update conda package
+    19  # Could not conda update all packages
+    23  # Could not conda install console and powershell shortcuts
+    29  # Could not deactivate conda
+    31  # Could not chocolatey upgrade all packages
+    37  # Could not update wsl kernel
+    41  # Could not apt update and apt upgrade in wsl
+    43  # Could not update Rust toolchain
+    47  # Could not upgrade Deno
 
 
 EOF
